4fe1706efd8842cbab3fac5694f95e18c65fbb63
Burak Can Kurtarır <burak.kurtarir35@gmail.com>
2024-07-26 00:35:42 +0300

    Added new summary

--- DotNetTodoRestApi/Controllers/WeatherForecastController.cs
+++ DotNetTodoRestApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DotNetTodoRestApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Very Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
