728ce8396fe3c39e587cce31ce1129d0c7484325
Burak Can Kurtarır <burak.kurtarir35@gmail.com>
2024-07-26 14:05:13 +0300

    Added Todo and Comment models.

---
+++ DotNetTodoRestApi/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNetTodoRestApi.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreateadOn { get; set; }
+        public int? TodoId { get; set; }
+        //Navigation
+        public Todo? Todo { get; set; }
+    }
+}
